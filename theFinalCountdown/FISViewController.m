5035740eaa776c32c6e6268cd274c102ffc95f7e
JimCampagno <jimcampagno@gmail.com>
2015-07-05 18:07:11 -0400

    initial commit

--- theFinalCountdown/FISViewController.m
+++ theFinalCountdown/FISViewController.m
@@ -10,31 +10,46 @@
 
 @interface FISViewController ()
 
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UILabel *timeLabel;
+@property (weak, nonatomic) IBOutlet UIButton *startButton;
+@property (weak, nonatomic) IBOutlet UIButton *pauseButton;
+@property (weak, nonatomic) IBOutlet UIView *lowerHalfView;
+
+- (IBAction)startButtonTapped:(id)sender;
+- (IBAction)pauseButtonTapped:(id)sender;
+
 @end
 
 @implementation FISViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-
-
-
-
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-
+    
+//    For right now, when you tap the start button the uiPickerView should be hidden, and the Label with a time should be shown. Don't worry about making it work just yet.
+//    When you rotate to landscape. It should just show the time label full screen. So like this:
 
 }
 
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
 }
 
+
+- (IBAction)startButtonTapped:(id)sender {
+    
+    self.timeLabel.hidden = NO;
+    self.datePicker.hidden = YES;
+    
+   
+}
+
+- (IBAction)pauseButtonTapped:(id)sender {
+    
+    self.timeLabel.hidden = YES;
+    self.datePicker.hidden = NO;
+    
+   
+    
+}
 @end
